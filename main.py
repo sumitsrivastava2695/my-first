b5321e1a038f043e828506e3c9cde9ee891b3ca2
sumitsrivastava2695 <sumit.work.2695@gmail.com>
2025-10-18 22:45:50 +0530

    Create main.py
    Added my first program

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+# My First GitHub Project 🎉
+
+name = input("What's your name? ")
+print(f"Hello, {name}! 👋 Welcome to GitHub!")
+
+# This is just a basic program to test GitHub.
+# You can modify this to do anything you like!
